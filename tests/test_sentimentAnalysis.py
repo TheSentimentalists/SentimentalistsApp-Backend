c369b2aab761370c4eb87eb69db7dfb5b740f206
anapotje <anapotje@gmail.com>
2020-10-12 17:22:41 +0100

    changes in test_sentimentAnalysis

--- tests/test_sentimentAnalysis.py
+++ tests/test_sentimentAnalysis.py
@@ -9,11 +9,15 @@
 import sentimentAnalysis as sentAnalysis   # The code to test
 
 def test_Polarity():
-    url = "https://www.bbc.co.uk/news/uk-54234084"
+    #url = "https://www.bbc.co.uk/news/uk-54234084"
+    url = 'http://sentimentalists-tests.s3-website.eu-west-2.amazonaws.com/today.html'
+
     analysed_text = sentAnalysis.sentimentAnalysis(url)
-    assert analysed_text.sentiment.polarity == 0.055258047508047504
+    assert analysed_text.sentiment.polarity == -1.0
 
 def test_Subjectivity():
-    url = "https://www.bbc.co.uk/news/uk-54234084"
+    #url = "https://www.bbc.co.uk/news/uk-54234084"
+    url = 'http://sentimentalists-tests.s3-website.eu-west-2.amazonaws.com/today.html'
+
     analysed_text = sentAnalysis.sentimentAnalysis(url)
-    assert analysed_text.sentiment.subjectivity == 0.4314774392274392
+    assert analysed_text.sentiment.subjectivity == 1.0

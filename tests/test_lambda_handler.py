727924074261f29fcac77b02fb503ec7bb95cefe
anapotje <anapotje@gmail.com>
2020-10-12 11:31:10 +0100

    code fixed in function test_lambda_handler.py

--- tests/test_lambda_handler.py
+++ tests/test_lambda_handler.py
@@ -1,13 +1,18 @@
 ## Function: test_lambda_handler.py
 ## Author: The Sentimentalists / Ana B Potje
-## Date: 05/10/2020
+## Date: 12/10/2020
 ##
 ## Tests the Function "lambda_handler.py"
 ## Uses PYTEST framework
 ###########################################################################################################
 
-import src.lambda_function as lf   # The code to test 
+import lambda_function as lf   # The code to test
 
-def test_URL_with_Score():
-    result_score = {'type': 'credibility', 'score': 95.0, 'source': 'Media Bias/Fact Check', 'category': 'UNS'}
-    assert lf.lambda_handler("https://www.bbc.co.uk/news/uk-54234084") == result_score
+def test_URL_with_Score(): 
+    result_score = [{"type": "credibility", "score": 95.0, "source": "Media Bias/Fact Check", "category": "UNS"}]
+    result_dict = eval(lf.lambda_handler({"url":"https://www.bbc.co.uk/news/uk-54234084"}, ""))
+    assert result_dict['results'] == result_score
+    
+def test_invalid_URL(): 
+    result_dict = eval(lf.lambda_handler({"url":"xxxxx"}, ""))
+    assert result_dict['error'] == 'Invalid URL'

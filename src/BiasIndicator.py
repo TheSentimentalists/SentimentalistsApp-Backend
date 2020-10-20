381eaa630a1c76c361d928aa6a63e4101f0ca7b9
Jyothsna <59825199+GCJyo@users.noreply.github.com>
2020-10-20 11:04:22 +0100

    Update BiasIndicator.py
    Updated with 'If' statement to address 'non-availability of credibility score'

--- src/BiasIndicator.py
+++ src/BiasIndicator.py
@@ -1,18 +1,25 @@
 #Function: getBiasScore
 #Input: credibility, polarity, subjectivity
 #Output: BiasScore - indicates possible Bias if an artcle based on credibility, polarity, subjectivity scores
+## Author: The Sentimentalists / G C Jyothsna
+## Date: 20/Oct/2020
 #Polarity: positive or negative emotive words and their positioing in a sentence
 #Subjectivity: Inversly proportanal to factual referances in article 
 #             (lack of factual referances result in higher subjectivity score)
 #Credibility: Rating given per channel/new paper by media observers, autonomus organizations
 
 def getBiasScore(credibility, polarity, subjectivity):
-
+            
     IncredibilityPercent = (100-credibility)
     PolarityPercent = (polarity)*100
     SubjectivityPercent = (subjectivity)*100
-
-    BiasIndicator = ((IncredibilityPercent + abs(PolarityPercent) + SubjectivityPercent)/3)
+    
+    if (credibility >= 0):
+        BiasIndicator = ((IncredibilityPercent + abs(PolarityPercent) + SubjectivityPercent)/3)
+        return BiasIndicator
+    else:
+        BiasIndicator = ((abs(PolarityPercent)) + SubjectivityPercent)/2
+        return BiasIndicator
 
     response = {'type':'bias','outcome':{'score':BiasIndicator}}
     return response

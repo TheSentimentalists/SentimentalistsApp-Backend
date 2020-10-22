6f67088d15ba9f49d8097ad36996489a56eac698
anapotje <anapotje@gmail.com>
2020-10-22 08:50:56 +0100

    spacyMatcher now uses  en_core_web_sm.load

--- tests/test_spacyMatcher.py
+++ tests/test_spacyMatcher.py
@@ -12,19 +12,16 @@ def test_Person():
 # PERSON - People, including fictional.
     url = 'https://www.theguardian.com/world/live/2020/oct/07/coronavirus-live-news-six-us-states-see-record-hospital-patients-facebook-deletes-trump-post'
     set_person = {'Covid', 'Donald Trump', 'Miller', 'Stephen Miller', 'Trump'}
-    #assert 1 == 1
     assert spacyMat.spacyMatcher(url, 'PERSON') == set_person
 
 def test_Org():
 #ORG - Companies, agencies, institutions, etc.
     url = 'https://www.theguardian.com/world/live/2020/oct/07/coronavirus-live-news-six-us-states-see-record-hospital-patients-facebook-deletes-trump-post'
     set_org = {'White House', 'Trump', 'the White House', 'Walter Reed'}
-    #assert 1 == 1
     assert spacyMat.spacyMatcher(url, 'ORG') == set_org
 
 def test_GPE():
 #GPE - Countries, cities, states.
     url = 'https://www.theguardian.com/world/live/2020/oct/07/coronavirus-live-news-six-us-states-see-record-hospital-patients-facebook-deletes-trump-post'
     set_gpe = {'US'}
-    #assert 1 == 1
     assert spacyMat.spacyMatcher(url, 'GPE') == set_gpe

7503725fe1a48af1f45d091da198c02f5cc054f1
Jyothsna <59825199+GCJyo@users.noreply.github.com>
2020-10-26 19:06:02 +0000

    Update getBiasScore.py

--- src/getBiasScore.py
+++ src/getBiasScore.py
@@ -1,13 +1,11 @@
 #Function: getBiasScore
 #Input: credibility, polarity, subjectivity from SentimentAnalysis, getCredibility
-#Output: BiasScore - indicates possible Bias if an artcle based on credibility, polarity, subjectivity scores
+#Output: BiasScore - indicates possible Bias if an article based on credibility, polarity, subjectivity scores
 ## Author: The Sentimentalists / G C Jyothsna
 ## Date: 20/Oct/2020
-#Polarity: positive or negative emotive words and their positioing in a sentence
-#Subjectivity: Inversly proportanal to factual referances in article 
-#             (lack of factual referances result in higher subjectivity score)
-#Credibility: Rating given per channel/new paper by media observers, autonomus organizations
-
+#Polarity: positive or negative emotive words and their positioning in a sentence#Subjectivity: Inversely proportional to factual references in article 
+#             (lack of factual references result in higher subjectivity score)
+#Credibility: Rating given per channel/new paper by media observers, autonomous organizations
 def getBiasScore(credibility, polarity, subjectivity):
             
     IncredibilityPercent = (100-credibility)

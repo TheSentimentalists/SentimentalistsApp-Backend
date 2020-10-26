ca2115be3da06eb8cfd63610233bd1a73f0d01be
Jamie Johnstone <j@miej.co>
2020-10-26 21:47:05 +0000

    Move to build script

---
+++ src/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install modules into ./cache/ folder
+if [ -f requirements.txt ]; then 
+pip3 install -r requirements.txt --target ./cache/; 
+mv ./cache/* .
+fi
+
+
+# Install dev requirements (like boto3) globally
+if [ -f build-requirements.txt ]; then pip3 install -r build-requirements.txt; fi
+
+# Run downloadPunkt.py to get that too...
+python3 downloadPunkt.py

cbbcafed59ba201a33bee9593aec4118d16b6c57
anapotje <anapotje@gmail.com>
2020-10-29 12:20:03 +0000

    included xray and TAGS description in spacyMatcher

--- src/spacyMatcher.py
+++ src/spacyMatcher.py
@@ -1,5 +1,5 @@
 ## Function: spacyMatcher
-## Input: TEXT and TAG (PERSON, ORG, GPE)
+## Input: TEXT and TAG (PERSON, ORG, GPE, etc)
 ## Output: Set with all names (of person, countries ..) found in the text
 ## Author: The Sentimentalists / Ana B Potje
 ## Date: 15/Oct/2020
@@ -8,18 +8,33 @@
 ##    PERSON - People, including fictional.
 ##    ORG - Companies, agencies, institutions, etc.
 ##    GPE - Countries, cities, states.
+##    PERCENT - Percentage, including ”%“.
+##    LANGUAGE - Any named language.
+##    DATE - Absolute or relative dates or periods.
+##    TIME - Times smaller than a day.
+##    LOC - Non-GPE locations, mountain ranges, bodies of water.
+##    NORP - Nationalities or religious or political groups.
+##    EVENT - Named hurricanes, battles, wars, sports events, etc.
+##    WORK_OF_ART - Titles of books, songs, etc.
+##    MONEY - Monetary values, including unit.
+##    QUANTITY - Measurements, as of weight or distance.
+##    ORDINAL - “first”, “second”, etc.
+##    CARDINAL - Numerals that do not fall under another type (not ordinal, quantity ..)
+##
 ## It will return a list with distinct values of all occurrences of that entity in the text.
 ###########################################################################################################
 
+from aws_xray_sdk.core import xray_recorder
+
+@xray_recorder.capture('spacyMatcher')
+
 def spacyMatcher(text, tag): 
     import spacy
-    #import en_core_web_sm
 
     ret_set = []
     if text != '-1':
         ### analysing the text 
         nlp = spacy.load("en_core_web_sm")
-        ###nlp = en_core_web_sm.load()
         doc = nlp(text)
     
         interestWords = [(ent.text, ent.label_) for ent in doc.ents]
